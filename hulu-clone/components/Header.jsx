e9513146577eb5dea084a6017f8b1452d7c51939
Baga-13 <osesebaga13@gmail.com>
2022-06-24 18:38:50 +0100

    setup done

---
+++ hulu-clone/components/Header.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image"
+
+function Header() {
+  return (
+    <header>
+        <Image src='https://links.papareact.com/ua6' width={200} height={100} />
+    </header>
+  )
+}
+
+export default Header
---
+++ hulu-clone/pages/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Header from '../components/Header'
+// import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Hulu</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* header */}
+      <Header />
+
+      {/* nav */}
+
+      {/* result */}
+
+      
+    </div>
+  )
+}
+
+export default Home
